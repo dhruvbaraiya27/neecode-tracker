0233c12a6e440345bba38dc74e621067a7b8cd23
Dhruv Baraiya <baraiya.d@northeastern.edu>
2026-03-14 14:35:47 -0400

    Add Contains Duplicate - Optimized - HashSet

---
+++ arrays-hashing/contains-duplicate/solution-2-optimized-hashset.java
@@ -0,0 +1,16 @@
+// Contains Duplicate - Optimized - HashSet
+// Time: O(N) | Space: O(N)
+// 
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums) {
+            if(set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}
